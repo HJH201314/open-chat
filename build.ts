90f5dda342ed368c25a19f8de5d02c287f660ec6
HJH201314 <1979396625@qq.com>
2024-01-31 23:03:26 +0800

    chore: build script

---
+++ build.ts
@@ -0,0 +1,74 @@
+import concurrently from "concurrently";
+import path from "path";
+import fs from "fs";
+
+const colorConsole = {
+  info: (...args: any[]) => {
+    console.log(
+      `\x1B[34m[open-chat] ${"%s".repeat(args.length)}\x1B[0m`,
+      ...args
+    );
+  },
+  warn: (...args: any[]) => {
+    console.log(
+      `\x1B[33m[open-chat] ${"%s".repeat(args.length)}\x1B[0m`,
+      ...args
+    );
+  },
+  success: (...args: any[]) => {
+    console.log(
+      `\x1B[32m[open-chat] ${"%s".repeat(args.length)}\x1B[0m`,
+      ...args
+    );
+  },
+  error: (...args: any[]) => {
+    console.log(
+      `\x1B[31m[open-chat] ${"%s".repeat(args.length)}\x1B[0m`,
+      ...args
+    );
+  },
+};
+
+// Build client and server using concurrently
+colorConsole.info("Start Building...");
+const { result } = concurrently(
+  [
+    { command: "pnpm run build:client", name: "client" },
+    { command: "pnpm run build:server", name: "server" },
+  ],
+  {
+    prefix: "build-{name}",
+    killOthers: ["failure"],
+    restartTries: 1,
+    cwd: path.resolve(__dirname, "./"),
+  }
+);
+
+result
+  .then(() => {
+    // Concurrent tasks are completed
+    colorConsole.info("✓ Build Completed.");
+
+    try {
+      // Copy file to ./dist
+      colorConsole.info("Start Copying Dist Files...");
+      fs.cpSync(
+        path.resolve(__dirname, "./client/dist"),
+        path.resolve(__dirname, "./dist/client"),
+        { recursive: true }
+      );
+      fs.cpSync(
+        path.resolve(__dirname, "./server/dist"),
+        path.resolve(__dirname, "./dist/server"),
+        { recursive: true }
+      );
+
+      colorConsole.success("✓ Copy Completed.");
+    } catch (e) {
+      colorConsole.error(e);
+    }
+  })
+  .catch((e: any) => {
+    // Error during concurrent tasks
+    colorConsole.error(e);
+  });
