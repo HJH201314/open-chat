275738313f90ef2ba323b95a2751f93bee9b81a0
HJH201314 <fcraft@qq.com>
2025-02-10 11:56:35 +0800

    chore: change server port to 9033

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -52,7 +52,7 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api\/cloud/, ''),
       },
       '/api/next': {
-        target: 'http://127.0.0.1:8080/',
+        target: 'http://127.0.0.1:9033/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/next/, ''),
       },
