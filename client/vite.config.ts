073067b5b8d08370843d5c2e2fd028ffdc1d335f
HJH201314 <fcraft@qq.com>
2025-03-08 14:40:51 +0800

    feat: refactor Vite configuration to support production-specific ESBuild options

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, ESBuildOptions, UserConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import { VitePWA } from 'vite-plugin-pwa';
@@ -10,59 +10,67 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools({
-      launchEditor: 'webstorm',
-    }),
-    sassDts({
-      enabledMode: ['development', 'production'],
-      global: {
-        generate: true,
-        outputFilePath: path.resolve(__dirname, './src/style.d.ts'),
-      },
-      sourceDir: path.resolve(__dirname, './src'),
-      outputDir: path.resolve(__dirname, './dist'),
-    }),
-    basicSsl(),
-    VitePWA(),
-  ],
-  css: {
-    // 对css的行为进行配置
-    // modules配置最终会丢给postcss modules
-    modules: {
-      // 是对css模块化的默认行为进行覆盖
-      localsConvention: 'camelCase', // 修改生成的配置对象的key的展示形式(驼峰还是中划线形式)
-      scopeBehaviour: 'local', // 配置当前的模块化行为是模块化还是全局化 (有hash就是开启了模块化的一个标志, 因为他可以保证产生不同的hash值来控制我们的样式类名不被覆盖)
-      generateScopedName: '[name]_[local]_[hash:5]', // 配置生成的类名的格式
-    },
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  server: {
-    port: 9035,
-    https: true,
-    proxy: {
-      '/api/cloud': {
-        target: 'http://127.0.0.1:3000/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/cloud/, ''),
-      },
-      '/api/next': {
-        target: 'http://127.0.0.1:9033/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/next/, ''),
-      },
-      '/api/origin': {
-        target: 'http://127.0.0.1:5000/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/origin/, ''),
+export default defineConfig(({ mode }): UserConfig => {
+  const esbuildOptions = {} as ESBuildOptions;
+  if (mode === 'production') {
+    // 打包时移除 console
+    esbuildOptions.pure = ['console.log'];
+  }
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+      vueDevTools({
+        launchEditor: 'webstorm',
+      }),
+      sassDts({
+        enabledMode: ['development', 'production'],
+        global: {
+          generate: true,
+          outputFilePath: path.resolve(__dirname, './src/style.d.ts'),
+        },
+        sourceDir: path.resolve(__dirname, './src'),
+        outputDir: path.resolve(__dirname, './dist'),
+      }),
+      basicSsl(),
+      VitePWA(),
+    ],
+    esbuild: esbuildOptions,
+    css: {
+      // 对css的行为进行配置
+      // modules配置最终会丢给postcss modules
+      modules: {
+        // 是对css模块化的默认行为进行覆盖
+        localsConvention: 'camelCase', // 修改生成的配置对象的key的展示形式(驼峰还是中划线形式)
+        scopeBehaviour: 'local', // 配置当前的模块化行为是模块化还是全局化 (有hash就是开启了模块化的一个标志, 因为他可以保证产生不同的hash值来控制我们的样式类名不被覆盖)
+        generateScopedName: '[name]_[local]_[hash:5]', // 配置生成的类名的格式
       },
     },
-  },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    server: {
+      port: 9035,
+      // https: true,
+      proxy: {
+        '/api/cloud': {
+          target: 'http://127.0.0.1:3000/',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api\/cloud/, ''),
+        },
+        '/api/next': {
+          target: 'http://127.0.0.1:9033/',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api\/next/, ''),
+        },
+        '/api/origin': {
+          target: 'http://127.0.0.1:5000/',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api\/origin/, ''),
+        },
+      },
+    },
+  };
 });
